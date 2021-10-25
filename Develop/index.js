ef036790e81b6129a7ad8b9238ec4f1fd3d10f06
Rhoda Evangelene <rhoda.evangelene@gmail.com>
2021-10-24 22:00:20 -0400

    Added generateMarkdown.js logic, integrated with index.js

--- Develop/index.js
+++ Develop/index.js
@@ -1,13 +1,104 @@
-// TODO: Include packages needed for this application
+const inquirer = require("inquirer");
+const fs = require("fs");
+const util = require("util");
+const generateMd = require("./utils/generateMarkdown");
+
+const writeFileAsync = util.promisify(fs.writeFile);
 
 // TODO: Create an array of questions for user input
-const questions = [];
+const questions = [
+    {
+        type: "input",
+        message: "Enter your GitHub user name?",
+        name: "username"
+    },
+    {
+        type: "input",
+        message: "Enter your email address?",
+        name: "email"
+    },
+    {
+        type: "input",
+        message: "Enter your project title?",
+        name: "title"
+    },
+    {
+        type: "input",
+        message: "Enter description of your project",
+        name: "description"
+    },
+    {
+        type: "list",
+        message: "Select an Open Source License?",
+        name: "license",
+        choices: [
+            "MIT",
+            "Unlicense",
+            "Apache 2.0",
+            "GNU v3",
+            "BSD 3-Clause",
+            "Mozilla Public License 2.0"
+        ]
+    },
+    {
+        type: "input",
+        message: "Enter the command to install dependencies?",
+        name: "installation",
+        default: "npm i"
+    },
+    {
+        type: "input",
+        message: "Enter the command to run tests?",
+        name: "tests",
+        default: "npm run test"
+    },
+    {
+        type: "input",
+        message: "Enter additional usage information:",
+        name: "usage"
+    },
+    {
+        type: "input",
+        message: "Describe contribution guidelines:",
+        name: "contribute"
+    },
+];
 
-// TODO: Create a function to write README file
-function writeToFile(fileName, data) {}
+const promptUser = () => {
+    return inquirer
+        .prompt(questions);
+}
 
-// TODO: Create a function to initialize app
-function init() {}
 
-// Function call to initialize app
+// function to write README file
+const writeToFile = (fileName, data) => {
+    var dir = './tmp';
+
+    if (!fs.existsSync(dir)) {
+        fs.mkdirSync(dir);
+    }
+    return writeFileAsync(fileName, data);
+}
+
+
+// function to initialize program
+const init = async () => {
+    try {
+        console.log("Welcome to the README generator.\nPlease answer the following questions:")
+
+        const answers = await promptUser();
+
+        const fileContent = generateMd(answers);
+
+        await writeToFile("./tmp/README.md", fileContent);
+
+        console.log("README file written to ./tmp/README.md successfully");
+
+    } catch (err) {
+        console.error("Error creating README. File not created.");
+        console.log(err);
+    }
+}
+
+// function call to initialize program
 init();
--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,18 +1,94 @@
-// TODO: Create a function that returns a license badge based on which license is passed in
+// Function that returns a license badge based on which license is passed in
 // If there is no license, return an empty string
-function renderLicenseBadge(license) {}
+const licBadges = {
+  "MIT": "https://img.shields.io/badge/License-MIT-blue.svg",
+  "Unlicense": "https://img.shields.io/badge/license-Unlicense-blue.svg",
+  "Apache 2.0": "https://img.shields.io/badge/License-Apache%202.0-blue.svg",
+  "GNU v3": "https://img.shields.io/badge/License-GPLv3-blue.svg",
+  "BSD 3-Clause": "https://img.shields.io/badge/License-BSD%203--Clause-blue.svg",
+  "Mozilla Public License 2.0": "https://img.shields.io/badge/License-MPL%202.0-blue.svg"
+}
+
+const licLinks = {
+  "MIT": "https://opensource.org/licenses/MIT",
+  "Unlicense": "http://unlicense.org/",
+  "Apache 2.0": "https://opensource.org/licenses/Apache-2.0",
+  "GNU v3": "https://www.gnu.org/licenses/gpl-3.0",
+  "BSD 3-Clause": "https://opensource.org/licenses/BSD-3-Clause",
+  "Mozilla Public License 2.0": "https://opensource.org/licenses/MPL-2.0"
+}
+
+
+function renderLicenseBadge(license) {
+  return licBadges[license];
+}
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  return licLinks[license];
+}
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+
+  return "[![License: " + license + "](" + renderLicenseBadge(license) + ")](" + renderLicenseLink(license) + ")"
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
+
+  data.licenseBadge = renderLicenseSection(data.license);
+
   return `# ${data.title}
+
+${data.licenseBadge}
+
+## Description
+
+${data.description}
+
+## Table of Contents
+
+* [Installation](#installation)
+* [Usage](#usage)
+* [License](#license)
+* [Contributing](#contributing)
+* [Tests](#tests)
+* [Questions](#questions)
+
+## Installation
+
+To install dependencies, run the following:
+
+\`
+${data.installation}
+\`
+
+## Usage
+
+${data.usage}
+
+## License
+
+This repository is licensed under the ${data.license} license.
+
+## Contributing
+
+${data.contribute}
+
+## Tests
+
+To run tests, run the following:
+
+\`
+${data.tests}
+\`
+
+## Questions
+
+Questions about this repository? Please contact me at [${data.email}](mailto:${data.email}). View more of my work in GitHub at [${data.username}](https://github.com/${data.username}) 
 
 `;
 }
